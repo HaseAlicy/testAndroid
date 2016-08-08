a8d1273550fcbce6dffc960deb4c0f85075ce706
HaseAlicy <haseyuukigouremu0516@gmail.com>
2016-08-08 23:51:57 +0900

    画面の上に点を表示

--- TestMap/app/src/main/java/com/example/alicy/testmap/MainActivity.java
+++ TestMap/app/src/main/java/com/example/alicy/testmap/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.alicy.testmap;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +17,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView)this.findViewById(R.id.imageView2);
+        Bitmap bitmap = Bitmap.createBitmap(1100,1600,Bitmap.Config.ARGB_8888);
+        int i;
+        Canvas canvas;
+        canvas = new Canvas(bitmap);
+
+
+        Paint paint;
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+
+        canvas.drawCircle(100,680,10,paint);//kaidan1
+        for(i=0;i<=15;i++) {
+            canvas.drawCircle(100+(i*60), 608, 10, paint);
+        }
+        canvas.drawCircle(100+((i-5)*60),680,10,paint);//elevetor1
+        canvas.drawCircle(100+((i-1)*60),680,10,paint);//kaidan2
+
+        for(i=0;i<=6;i++) {
+            canvas.drawCircle(700, 680+(i*60), 10, paint);
+        }
+        canvas.drawCircle(760,680+(60*6),10,paint);//elevetor2
+        canvas.drawCircle(100, 1050, 10, paint);//kaidan3
+        for(i=0;i<=15;i++) {
+            canvas.drawCircle(100+(i*60), 1110, 10, paint);
+        }
+        canvas.drawCircle(1000, 1050, 10, paint); //kaidan4
+
+
+        //canvas.drawText("67.8",90,90,paint);
+
+        imageView.setImageBitmap(bitmap);
     }
 
 
